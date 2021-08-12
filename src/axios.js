6ebf460b1a85fc162519760c52434e865248b606
Mdanialzadeh <mdanialzadeh10@gmail.com>
2021-08-11 20:17:14 -0400

    axios url updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/megarian-carpet/us-central1/api'
+    baseURL: 'https://us-central1-megarian-carpet.cloudfunctions.net/api'
+
 });
 
 export default instance;
+
+//'http://localhost:5001/megarian-carpet/us-central1/api'
